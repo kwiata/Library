594cb3f0756b74ca5b637475385c79c497236cf5
Agnieszka Kwiatkowska <a92kwiatkowska@gmail.com>
2019-09-09 13:50:20 +0200

    Biblioteka v0.1 first book added

---
+++ src/Library.java
@@ -0,0 +1,21 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.1";
+
+        String title = "Sapiens. Od zwierząt do bogów";
+        String author = "Yuval Noah Harari";
+        int releaseDate = 2014;
+        int pages = 540;
+        String publisher = "Wydawnictwo Literackie";
+        String isbn = "978-83-08-06814-4";
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece: ");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
